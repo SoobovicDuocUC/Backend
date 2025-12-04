223f3df1d2f63a78f4850e979e67a07c3c07aa23
SoobovicDuocUC <seba.arayar@duocuc.cl>
2025-12-04 06:12:42 -0300

    Backend Full de FullStack + Algunas cosas

--- Backend/src/main/java/com/huertohogar/Backend/config/OpenApiConfig.java
+++ Backend/src/main/java/com/huertohogar/Backend/config/OpenApiConfig.java
@@ -3,7 +3,9 @@ package com.huertohogar.Backend.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,8 +13,12 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "API Huerto Hogar",
                 version = "1.0",
-                description = "Documentación de la API para la gestión de productos y usuarios de la tienda Huerto Hogar."
-        )
+                description = "Documentación de la API para la gestión de productos y usuarios."
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Servidor Local")
+        },
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
         name = "bearerAuth",
